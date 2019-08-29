34c69c784b7f61b8fe031160cae7bdd01f0a846c
Ethan Gruis <egruis98@gmail.com>
2019-08-29 10:11:13 -0700

    inserted ANOTHER println

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println()"My second hello world!");
+        System.out.println("My second hello world!");
+        System.out.println("another println");
+        System.out.println("ANOTHER one");
     }
 }
